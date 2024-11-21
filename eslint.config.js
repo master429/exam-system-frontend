31c48a4bf8e51b927b120c317b5b3607202a8cfa
liuqingpo <1432003391@qq.com>
2024-11-21 12:00:04 +0800

    试卷编辑器的功能

--- eslint.config.js
+++ eslint.config.js
@@ -25,6 +25,7 @@ export default tseslint.config(
       ],
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-unsafe-function-type": "off",
+      "@typescript-eslint/no-unused-vars": ["off"],
     },
   }
 );
--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,9 @@ import { Login } from "./pages/Login";
 import { Register } from "./pages/Register";
 import { UpdatePassword } from "./pages/UpdatePassword";
 import { ExamList } from "./pages/ExamList";
+import { Edit } from "./pages/Edit";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 
 const routes = [
   {
@@ -22,6 +25,10 @@ const routes = [
     path: "update_password",
     element: <UpdatePassword />,
   },
+  {
+    path: "edit/:id",
+    element: <Edit />,
+  },
 ];
 const router = createBrowserRouter(routes, {
   future: {
@@ -38,10 +45,12 @@ const root = ReactDOM.createRoot(
 );
 
 root.render(
-  <RouterProvider
-    future={{
-      v7_startTransition: true,
-    }}
-    router={router}
-  />
+  <DndProvider backend={HTML5Backend}>
+    <RouterProvider
+      future={{
+        v7_startTransition: true,
+      }}
+      router={router}
+    />
+  </DndProvider>
 );
